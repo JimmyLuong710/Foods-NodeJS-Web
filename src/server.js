4250df188b2f64ce752a5c15ed5c3166fbb50240
vanadi <vinh07102002@gmail.com>
2022-07-13 00:44:59 +0700

    heroku2

--- src/server.js
+++ src/server.js
@@ -25,13 +25,6 @@ app.use(cors({
   origin
 }))
 
-app.all('*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
-
 // CONNECT TO DB
 connectDB();
 
